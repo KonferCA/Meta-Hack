967c933ac5d4207ce484edd15c8c1946680d058e
jc <46619361+juancwu@users.noreply.github.com>
2024-11-23 19:09:59 -0500

    fix path

--- backend/rlfh.py
+++ backend/rlfh.py
@@ -1,8 +1,8 @@
 from llama import Llama
 
 generator = Llama.build(
-    ckpt_dir="../.llama/checkpoints/Llama-3.2-11B-Vision",
-    tokenizer_path="../.llama/checkpoints/Llama-3.2-11B-Vision/tokenizer.model",
+    ckpt_dir="../.llama/checkpoints/Llama3.2-11B-Vision",
+    tokenizer_path="../.llama/checkpoints/Llama3.2-11B-Vision/tokenizer.model",
     max_seq_len=128,
     max_batch_size=4,
 )
