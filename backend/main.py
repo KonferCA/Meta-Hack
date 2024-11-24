0c7d5f51647a3f9224c103185c75f132e1f1184b
jc <46619361+juancwu@users.noreply.github.com>
2024-11-24 06:35:34 -0500

    allow all origins

--- backend/main.py
+++ backend/main.py
@@ -59,7 +59,7 @@ app = FastAPI()
 # configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # frontend URL
+    allow_origins=["*"],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
